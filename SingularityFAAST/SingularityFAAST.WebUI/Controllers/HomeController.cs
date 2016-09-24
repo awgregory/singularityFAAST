9ef160ac6c54fe5ea7420a8850c326405aeb38ef
Adrian Gregory <adrianwgregory2@gmail.com>
2016-09-24 13:51:26 -0400

    Added HomeController and Layout Page added Navbar Links

---
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingularityFAAST.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
