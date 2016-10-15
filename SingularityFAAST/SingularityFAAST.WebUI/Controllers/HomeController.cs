e9991bcb34b84130b3acc77e5dfe3cf18cbbd27d
Adrian Gregory <adrianwgregory2@gmail.com>
2016-10-15 12:49:12 -0400

    Testing Azure CI

--- SingularityFAAST/SingularityFAAST.WebUI/Controllers/HomeController.cs
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SingularityFAAST.WebUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,37 @@ namespace SingularityFAAST.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static int id = 0;
+
+        private static List<Client> clients = new List<Client>()
+        {
+            new Client {ClientID = 1, LastName= "Stevens", FirstName="Bob",
+                Address= "123 Main St", StateAbbr= "FL", ZipCode=32210,
+                Category ="Individual with a Disability"},
+
+            new Client {ClientID = 2, LastName= "Brown", FirstName="Rocco",
+                Address= "64 Side St", StateAbbr= "FL", ZipCode=32256,
+                Category ="Family guardian or authorized rep"},
+
+            new Client {ClientID = 3, LastName= "Teske", FirstName="David",
+                Address= "PO Box 909", StateAbbr= "NC", ZipCode=28711,
+                Category ="Family guardian or authorized rep"},
+
+            new Client {ClientID = 4, LastName= "Stayer", FirstName="Bruce",
+                Address= "134 Deanna Drive", StateAbbr= "FL", ZipCode=33852,
+                Category ="Family guardian or authorized rep"},
+        };
+        
         // GET: Client
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Client()
+        {
+            return View(clients);
         }
     }
 }
---
+++ SingularityFAAST/SingularityFAAST.WebUI/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingularityFAAST.WebUI.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        //public string Company { get; set; } // Keep? All seem empty
+        public string Address { get; set; }
+        public string StateAbbr { get; set; }
+        public int ZipCode { get; set; }
+        public string Category { get; set; }
+    }
+}
