bdb89fa4b84b32c95f8881911c724e1d555d1b4e
Adrian Gregory <adrianwgregory2@gmail.com>
2016-10-17 21:59:49 -0400

    Added Data Annotations to the Client Model

--- SingularityFAAST/SingularityFAAST.WebUI/Models/Client.cs
+++ SingularityFAAST/SingularityFAAST.WebUI/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,27 @@ namespace SingularityFAAST.WebUI.Models
     public class Client
     {
         public int ClientID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-        //public string Company { get; set; } // Keep? All seem empty
+        [Required(ErrorMessage = "Please enter an address")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter a 2 digit state abbreviation")]
+        [Display(Name = "State Abbr")]
         public string StateAbbr { get; set; }
+
+        [Required(ErrorMessage = "Please enter a zip code")]
+        [Display(Name = "Zip Code")]
         public int ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category")]
         public string Category { get; set; }
     }
 }
