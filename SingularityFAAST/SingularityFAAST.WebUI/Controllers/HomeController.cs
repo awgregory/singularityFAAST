69c02972ccd3e28a0f3e7d41305b67a266404fd5
Adrian Gregory <adrianwgregory2@gmail.com>
2016-10-23 16:57:50 -0400

    Remove unecessary usings

--- SingularityFAAST/SingularityFAAST.WebUI/Controllers/HomeController.cs
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using SingularityFAAST.WebUI.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SingularityFAAST.WebUI.Controllers
