721fe4934c4ecbdb326d820603efc2e717bec9b8
Adrian Gregory <adrianwgregory2@gmail.com>
2016-10-23 21:11:31 -0400

    Added InventoryItemCategory attribute to InventoryItems Table

--- SingularityFAAST/SingularityDB/dbo/InventoryItems.sql
+++ SingularityFAAST/SingularityDB/dbo/InventoryItems.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[InventoryItems]
 (
 	[InventoryItemId] INT NOT NULL PRIMARY KEY, 
-    [DatePurchased] DATETIME NOT NULL
+    [DatePurchased] DATETIME NOT NULL, 
+    [InventoryItemCategory] VARCHAR(50) NOT NULL
 )
