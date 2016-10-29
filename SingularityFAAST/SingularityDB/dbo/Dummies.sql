bd5e167ee29cbefa9b06fd200917186bfdfcc019
Nick <necampanini@gmail.com>
2016-10-29 13:08:32 -0400

    example and reference work on dummy data - literally

---
+++ SingularityFAAST/SingularityDB/dbo/Dummies.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Dummies]
+(
+	[DummyId] INT NOT NULL IDENTITY,
+	[Name] VARCHAR(20) NOT NULL,
+	[Age] INT NOT NULL,
+	[Description] VARCHAR(20) NULL,
+	--[DummyTypeId] INT NOT NULL
+
+	CONSTRAINT [PK_Dummies] PRIMARY KEY (DummyId)
+	--CONSTRAINT [FK_Dummies_DummyTypes] FOREIGN KEY ([DummyTypeId])
+	
+)
