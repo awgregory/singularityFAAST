a00a7141bd5755514249e5da98626b9e22388c61
Jonathan Ebert <jebert142@gmail.com>
2016-10-29 12:07:36 -0400

    inventory tables restructured. Damages attribute added

--- SingularityFAAST/SingularityDB/dbo/InventoryItemCategories.sql
+++ SingularityFAAST/SingularityDB/dbo/InventoryItemCategories.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [dbo].[InventoryItemCategories]
 (
 	[InventoryCategoryId] INT NOT NULL IDENTITY(1,1), 
-    [InvCategoryName] VARCHAR(50) NOT NULL,
+    [CategoryName] VARCHAR(50) NOT NULL,
+	--[SubCategory] VARCHAR(50) NULL, 
 
-	CONSTRAINT [PK_InventoryCategories] PRIMARY KEY ([InventoryCategoryId])
+    CONSTRAINT [PK_InventoryCategories] PRIMARY KEY ([InventoryCategoryId]),
+	--CONSTRAINT	[FK_InventoryItemSubCategory]	FOREIGN KEY	([SubCategory])
+	--	REFERENCES	dbo.InventoryItemSubCategories (InvSubCategoryName)
 )
--- SingularityFAAST/SingularityDB/dbo/InventoryItems.sql
+++ SingularityFAAST/SingularityDB/dbo/InventoryItems.sql
@@ -2,22 +2,22 @@
 (
 	[InventoryItemId]		INT				NOT NULL	IDENTITY(1,1), 
     [DatePurchased]			DATETIME		NOT NULL, 
-    [InventoryItemCategory] VARCHAR(50)		NOT NULL, 
+    [InventoryCategoryId] INT		NOT NULL, 
     [ItemName]				VARCHAR(80)		NOT NULL, 
     [PricePaid]				MONEY			NOT NULL,
-	[RetailCost]			MONEY			NOT NULL, 
-    [SubCategory]			VARCHAR(50)		NULL, 
-    [ModelOfItem]			VARCHAR(50)		NULL, 
+	[RetailCost]			MONEY			NOT NULL,
+    [ModelName]				VARCHAR(50)		NULL, 
     [Location]				VARCHAR(50)		NULL, 
     [Availability]			BIT				NOT NULL, 
 --serial numbers have a max size of 14 characters and can contain numbers letters and special characters
 	[SerialNumber]			VARCHAR(14)		NOT NULL , 
     [Description]			VARCHAR(MAX)	NULL, 
     [Accessories]			VARCHAR(150)	NULL,
+	[Damages]				VARCHAR(200)	NOT NULL, 
 
-	CONSTRAINT	[PK_InventoryItemId]		PRIMARY KEY	([InventoryItemId]),
-	CONSTRAINT	[FK_InventoryItemCategory]	FOREIGN KEY	([InventoryItemCategory])
-		REFERENCES	dbo.InventoryItemCategories (InvCategoryName),
-	CONSTRAINT	[FK_InventoryItemSubCategory]	FOREIGN KEY	([SubCategory])
-		REFERENCES	dbo.InventoryItemSubCategories (InvSubCategoryName)
+    CONSTRAINT	[PK_InventoryItemId]		PRIMARY KEY	([InventoryItemId]),
+	CONSTRAINT	[FK_InventoryItems_InventoryItemCategories]	FOREIGN KEY	([InventoryCategoryId])
+		REFERENCES	dbo.InventoryItemCategories ([InventoryCategoryId])
+	--CONSTRAINT	[FK_InventoryItemSubCategory]	FOREIGN KEY	([SubCategory])
+	--	REFERENCES	dbo.InventoryItemSubCategories (InvSubCategoryName)
 )
