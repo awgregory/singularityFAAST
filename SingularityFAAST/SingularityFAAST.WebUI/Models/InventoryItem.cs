f81e753bd386649f87ee0a0f26f23b9b00ce4b22
Jonathan Ebert <jebert142@gmail.com>
2016-11-06 15:18:09 -0500

    updated model views and db for inventory

--- SingularityFAAST/SingularityFAAST.WebUI/Models/InventoryItem.cs
+++ SingularityFAAST/SingularityFAAST.WebUI/Models/InventoryItem.cs
@@ -5,27 +5,24 @@ using System.Web;
 
 namespace SingularityFAAST.WebUI.Models
 {
-    /**InventoryItem contains item number, name, serial number, retail cost, category, sub category (optional), manufacturer, model, purchase date,
-        cost paid, location, description, accessories, additional files, and availability for and device at Hope Haven's FAAST Demonstration center**/
+    /**InventoryItem contains item id, date purchased, Category Id (FK), item name, 
+     * price paid, retail cost, model name, location, serial number, description, 
+     * accessories, damages, and availability for and device at Hope Haven's FAAST Demonstration center**/
 
     public class InventoryItem
     {
-        public string Number { get; set; }  //should auto generate new numbers for new items
-        public string Name { get; set; }
-        public double SerialNumber { get; set; }
+        public int  InventoryItemId { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public int InventoryCategoryId { get; set; }
+        public string ItemName { get; set; }
+        public decimal PricePaid { get; set; }
         public decimal RetailCost { get; set; }
-        public string Category { get; set; }
-        public string SubCategory { get; set; }
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
-        public DateTime PurchaseDate { get; set; }
-        public decimal CostPaid { get; set; }
+        public string ModelName { get; set; }
         public string Location { get; set; }
+        public bool Availability { get; set; }
+        public string SerialNumber { get; set; }
         public string Description { get; set; }
         public string Accessories { get; set; }
-
-        //Attribute(s) for additional files or images.
-
-        public bool Availability { get; set; }  //is this device checked out or not
+        public string Damages { get; set; }
     }
 }
