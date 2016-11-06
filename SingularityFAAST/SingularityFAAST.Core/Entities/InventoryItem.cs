3e2cf019f51fff97f83ded0e05b94b9c8d48bd06
Jonathan Ebert <jebert142@gmail.com>
2016-11-06 15:40:10 -0500

    changes to inventoryItem model

--- SingularityFAAST/SingularityFAAST.Core/Entities/InventoryItem.cs
+++ SingularityFAAST/SingularityFAAST.Core/Entities/InventoryItem.cs
@@ -1,7 +1,23 @@
-﻿namespace SingularityFAAST.Core.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SingularityFAAST.Core.Entities
 {
     public class InventoryItem
     {
-        // stuff
+        [Key]
+        public int InventoryItemId { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public int InventoryCategoryId { get; set; }
+        public string ItemName { get; set; }
+        public decimal PricePaid { get; set; }
+        public decimal RetailCost { get; set; }
+        public string ModelName { get; set; }
+        public string Location { get; set; }
+        public bool Availability { get; set; }
+        public string SerialNumber { get; set; }
+        public string Description { get; set; }
+        public string Accessories { get; set; }
+        public string Damages { get; set; }
     }
 }
