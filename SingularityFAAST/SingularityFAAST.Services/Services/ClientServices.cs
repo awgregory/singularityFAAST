0bd6f6ff8cf73c3e62d84f2306c67c9051529748
Adrian Gregory <adrianwgregory2@gmail.com>
2016-12-02 11:40:38 -0500

    Clean up

--- SingularityFAAST/SingularityFAAST.Services/Services/ClientServices.cs
+++ SingularityFAAST/SingularityFAAST.Services/Services/ClientServices.cs
@@ -7,21 +7,17 @@ namespace SingularityFAAST.Services.Services
 {
     public class ClientServices
     {
-        //often - will create service layer specific model for what we are returning
-        //we can safely use Client because it reisdes in Core
+        
         public IList<Client> GetAllClients()
         {
             using (var context = new SingularityDBContext())
             {
-                //var clients = context.Clients.Where(client => client.Age > 50);   Example
-
-                //grab clients in Clients table - NOT A LIST YET
+                
+                
                 var clients = context.Clients;
-
-                //instantiate into list of concrete client objects (c# objs)
+                
                 var clientList = clients.ToList();
-
-                //return them
+                
                 return clientList;
             }
         }
@@ -30,7 +26,7 @@ namespace SingularityFAAST.Services.Services
         {
             using (var context = new SingularityDBContext())
             {
-                context.Clients.Add(client);   //that doesn't save just yet
+                context.Clients.Add(client);
 
                 var rowsAffected = context.SaveChanges();
             }
