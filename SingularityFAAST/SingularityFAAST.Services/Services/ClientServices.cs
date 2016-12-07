ca9acd5cd0c64be7b6877f895a7d46aceadbc38d
Adrian Gregory <adrianwgregory2@gmail.com>
2016-12-07 02:02:31 -0500

    Correcting csproject and other issues after merging develop into feature/AddClient

--- SingularityFAAST/SingularityFAAST.Services/Services/ClientServices.cs
+++ SingularityFAAST/SingularityFAAST.Services/Services/ClientServices.cs
@@ -1,5 +1,6 @@
 ﻿using SingularityFAAST.Core.Entities;
 using SingularityFAAST.DataAccess.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,8 +13,7 @@ namespace SingularityFAAST.Services.Services
         {
             using (var context = new SingularityDBContext())
             {
-                
-                
+                           
                 var clients = context.Clients;
                 
                 var clientList = clients.ToList();
@@ -26,6 +26,8 @@ namespace SingularityFAAST.Services.Services
         {
             using (var context = new SingularityDBContext())
             {
+                client.DateCreated = DateTime.Now;  // Manipulating the client object is done before saving to Db
+
                 context.Clients.Add(client);
 
                 var rowsAffected = context.SaveChanges();
