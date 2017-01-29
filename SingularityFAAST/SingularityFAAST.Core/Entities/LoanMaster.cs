88949fdcdaa7d749bd614664d8efa15ee5b3ca32
KateYou <kyoungbe@gmail.com>
2017-01-29 17:00:55 -0500

    Changes to Singularity DB Loan tables and seed from group meeting with Nick 1/29/17

--- SingularityFAAST/SingularityFAAST.Core/Entities/LoanMaster.cs
+++ SingularityFAAST/SingularityFAAST.Core/Entities/LoanMaster.cs
@@ -5,45 +5,15 @@ namespace SingularityFAAST.Core.Entities
 {
     public class LoanMaster
     {
-        //[Key]
-        //public int LoanDetailId { get; set; }
 
         [Key]
         public int LoanMasterId { get; set; }
-
-        //public DateTime LoanDate{ get; set; }
-
-        //public int LoanDuration { get; set; }
-
-        //public string Purpose { get; set; }
-
-        //public string PurposeType { get; set; }
-
-        // public string ClientOutcome { get; set; }
-
-        //public string Notes { get; set; }
 
         public DateTime DateCreated { get; set; }
 
         public bool IsActive { get; set; }
 
         public int ClientId { get; set; }
-
-        public int LoanCategoryId { get; set; }
-
-        public int InventoryItemId { get; set; }
-
-        //public int InventoryItemId1 { get; set; }
-
-        //public int InventoryItemId2 { get; set; }
-
-        //public int InventoryItemId3 { get; set; }
-
-        //public int InventoryItemId4 { get; set; }
-
-        //public int InventoryItemId5 { get; set; }
-
-        //public int InventoryItemId6 { get; set; }
 
         public string LoanNumber { get; set; }
     }
--- SingularityFAAST/SingularityFAAST.DataAccess/Contexts/SingularityDBContext.cs
+++ SingularityFAAST/SingularityFAAST.DataAccess/Contexts/SingularityDBContext.cs
@@ -1,6 +1,4 @@
 ﻿using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Net.Http;
 using SingularityFAAST.Core.Entities;
 
 namespace SingularityFAAST.DataAccess.Contexts
@@ -10,7 +8,7 @@ namespace SingularityFAAST.DataAccess.Contexts
     {
         public DbSet<Client> Clients { get; set; }
         public DbSet<InventoryItem> InventoryItems { get; set; }
-        //public DbSet<Loan> Loans { get; set; }
         public DbSet<LoanMaster> LoanMasters { get; set; }
+        public DbSet<LoanDetail> LoanDetails { get; set; }
     }
 }
