47e7acf5a2b70c894e98d2aa4aabc8e4e735c186
Your Name <you@example.com>
2017-02-08 18:49:50 -0500

    Work in progress - Reports

--- SingularityFAAST/SingularityFAAST.Services/Services/ReportingServices.cs
+++ SingularityFAAST/SingularityFAAST.Services/Services/ReportingServices.cs
@@ -1,6 +1,38 @@
-﻿namespace SingularityFAAST.Services.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SingularityFAAST.Core.Entities;
+using SingularityFAAST.DataAccess.Contexts;
+
+namespace SingularityFAAST.Services.Services
 {
     public class ReportingServices
     {
+        private readonly SingularityDBContext _dbContext = new SingularityDBContext();
+
+        public IList<Loan> GetAllLoans()
+        {
+            IList<Loan> loans = _dbContext.Loans.ToList();
+
+            //var allActiveLoans = loans.Where(loan => loan.IsActive == true);
+
+            //IList<bool> oldLoans = loans
+            //    .Where(loan => loan.DateCreated < DateTime.UtcNow)
+            //    .Select(filteredLoan => filteredLoan.IsActive)
+            //    .ToList();
+
+            return loans;
+        }
+
+        public IList<Loan> GetDateFilteredLoans(DateTime startDate, DateTime endDate)
+        {
+            IList<Loan> filteredLoans = _dbContext.Loans
+                .Where(loan => loan.DateCreated > startDate
+                            && loan.DateCreated < endDate)
+                .ToList();
+
+            return filteredLoans;
+            ;
+        }
     }
 }
---
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/ReportController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using SingularityFAAST.Core.Entities;
+using SingularityFAAST.Services.Services;
+
+namespace SingularityFAAST.WebUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ReportingServices _reportingServices;
+
+        public ReportController()
+        {
+            _reportingServices = new ReportingServices();
+        }
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Loans
+        public ActionResult LoansReport()
+        {
+            var startDate = DateTime.UtcNow.AddYears(-1);
+            var endDate = DateTime.UtcNow;
+
+            IList<Loan> loans = _reportingServices.GetDateFilteredLoans(startDate, endDate);
+
+            return View(loans);
+        }
+
+        public ActionResult FilterLoansByDate(DateTime startDate, DateTime endDate)
+        {
+            IList<Loan> loans = _reportingServices.GetDateFilteredLoans(startDate, endDate);
+
+            return View("LoansReport", loans);
+        }
+        #endregion
+
+        #region Inventory Reports
+
+        #endregion
+
+        #region Clients Reports
+        //todo client reports
+        #endregion
+    }
+}
