d08d2ad50b115d12f1bc59cb6fe4a1cf07790306
KateYou <kyoungbe@gmail.com>
2017-02-23 22:07:29 -0500

    commit to change branches

--- SingularityFAAST/SingularityFAAST.Services/Services/LoanMasterServices.cs
+++ SingularityFAAST/SingularityFAAST.Services/Services/LoanMasterServices.cs
@@ -128,11 +128,10 @@ namespace SingularityFAAST.Services.Services
                 //Client client = new Client();
 
                 var clients = from c in context.Clients
-
-                              join lm in context.LoanMasters
-                              on c.ClientId equals lm.ClientId
+                              join l in context.LoanMasters
+                              on c.ClientID equals l.ClientId
                               join ld in context.LoanDetails
-                              on lm.LoanMasterId equals ld.LoanMasterId
+                              on l.LoanMasterId equals ld.LoanMasterId
                               join i in context.InventoryItems
                               on ld.InventoryItemId equals i.InventoryItemId
 
