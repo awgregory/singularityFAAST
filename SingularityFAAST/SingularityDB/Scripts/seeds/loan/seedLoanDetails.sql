02cf1ff7c1a0583a1ec3276ed30eec915a12910a
Adrian Gregory <adrianwgregory2@gmail.com>
2017-03-23 18:44:03 -0400

    Add seedLoanDetails

--- SingularityFAAST/SingularityDB/Scripts/seeds/loan/seedLoanDetails.sql
+++ SingularityFAAST/SingularityDB/Scripts/seeds/loan/seedLoanDetails.sql
@@ -1,4 +1,6 @@
-﻿IF OBJECT_ID('tempdb..#tempLoanDetails') IS NOT NULL DROP TABLE #tempLoanDetails 
+﻿
+
+IF OBJECT_ID('tempdb..#tempLoanDetails') IS NOT NULL DROP TABLE #tempLoanDetails 
 
 CREATE TABLE #tempLoanDetails
 (
@@ -16,32 +18,38 @@ CREATE TABLE #tempLoanDetails
 	CONSTRAINT [FK_LoanDetails_InventoryItems] FOREIGN KEY ([InventoryItemId]) REFERENCES dbo.InventoryItems ([InventoryItemId]),
 )
 
-INSERT INTO #tempLoanDetails([DateCreated],[ClientId],[IsActive],[LoanNumber])
-     VALUES						(GETDATE(),		3,			1,			15499),
-								(GETDATE(),		6,			1,			24700),
-								(GETDATE(),2, 1, 3454),
-								(GETDATE(),9, 1, 457),
-								(GETDATE(),12, 1, 2222),
-								(GETDATE(),7, 1, 46859),
-								(GETDATE(),11, 1, 23547),
-								(GETDATE(),13, 1, 35477),
-								(GETDATE(),4, 1, 54686)
+--LoanDetailId is auto
+INSERT INTO #tempLoanDetails([LoanMasterId],[LoanDate],[LoanDuration],[InventoryItemId],[Purpose],[PurposeType],[ClientOutcome],[Notes])
+     VALUES
+		   (1,GETDATE(),28,1,'Assist in decision making (device trial or evaluation)','Education','AT will meet needs','two charger cords & one charger'),
+		   (2,GETDATE(),28,28,'Assist in decision making (device trial or evaluation)','Education','AT will meet needs','Ms. Sowada checked out item for client whose name is John Kinsler. They are from Brooks Rehab.'),
+		   (3,GETDATE(),28,2,'Assist in decision making (device trial or evaluation)','Employment','AT will meet needs','AT left message with client at phone number and emailed her at jaydensamir@gmail.com to return iPAD on 8/31/16'),
+		   (3,GETDATE(),28,19,'Assist in decision making (device trial or evaluation)','Education','AT will meet needs',''),
+		   (4,GETDATE(),28,23,'Assist in decision making (device trial or evaluation)','Community Living','AT will meet needs','client also has Bug Vibrator')
+		   
+
 
 MERGE dbo.LoanDetails AS target
 USING #tempLoanDetails AS source
 	ON source.LoanNumber = target.LoanNumber
 
 WHEN NOT MATCHED THEN 
-	INSERT (DateCreated, ClientId, IsActive, LoanNumber)
-	VALUES (source.DateCreated, source.ClientId, source.IsActive, source.LoanNumber)
+	INSERT ([LoanMasterId],[LoanDate],[LoanDuration],[InventoryItemId],[Purpose],[PurposeType],[ClientOutcome],[Notes])
+	VALUES (source.LoanMasterId, source.LoanDate, source.LoanDuration, source.InventoryItemId, source.Purpose, source.PurposeType, source.ClientOutcome, source.Notes)
+
 
 WHEN MATCHED THEN UPDATE 
 SET 
 	
-	target.DateCreated = source.DateCreated,
-	target.ClientId = source.ClientId,
-	target.IsActive = source.IsActive,
-	target.MiddleInitial = source.MiddleInitial;
+	target.LoanMasterId = source.LoanMasterId,
+	target.LoanDate = source.LoanDate,
+	target.LoanDuration = source.LoanDuration,
+	target.InventoryItemId = source.InventoryItemId,
+	target.Purpose = source.Purpose,
+	target.PurposeType = source.PurposeType,
+	target.ClientOutcome = source.ClientOutcome,
+	target.Notes = source.Notes;
+
 	
-	DROP TABLE #tempClients
+	DROP TABLE #tempLoanDetails
 
