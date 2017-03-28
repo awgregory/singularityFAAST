eba0792f02cbe6f45f861148f8e383b54d7ad165
KateYou <kyoungbe@gmail.com>
2017-03-27 21:47:02 -0400

    Updates to dates in loan tables

--- SingularityFAAST/SingularityDB/Scripts/seeds/seedLoanMaster.sql
+++ SingularityFAAST/SingularityDB/Scripts/seeds/seedLoanMaster.sql
@@ -12,15 +12,15 @@ CREATE TABLE #tempLoanMasters
 
 --LoanMasterId is auto
 INSERT INTO #tempLoanMasters([DateCreated],[ClientId],[IsActive],[LoanNumber])
-     VALUES						(GETDATE(),		1,			1,			00001),
-								(GETDATE(),		1,			1,			00002),
-								(GETDATE(),		1,			0,			00003),
-								(GETDATE(),		2,			1,			00004),
-								(GETDATE(),		3,			1,			00005),
-								(GETDATE(),		4,			0,			00006),
-								(GETDATE(),		2,			1,			00007),
-								(GETDATE(),		3,			1,			00008),
-								(GETDATE(),		4,			1,			00009)
+     VALUES						(N'2017-01-21 00:00:00',		1,			1,			00001),
+								(N'2017-03-01 00:00:00',		1,			1,			00002),
+								(N'2016-06-12 00:00:00',		1,			0,			00003),
+								(N'2017-03-22 00:00:00',		2,			1,			00004),
+								(N'2017-02-23 00:00:00',		3,			1,			00005),
+								(N'2016-08-17 00:00:00',		4,			0,			00006),
+								(N'2017-02-23 00:00:00',		2,			1,			00007),
+								(N'2017-03-24 00:00:00',		3,			1,			00008),
+								(N'2017-03-17 00:00:00',		4,			1,			00009)
 
 
 MERGE dbo.LoanMasters AS target
