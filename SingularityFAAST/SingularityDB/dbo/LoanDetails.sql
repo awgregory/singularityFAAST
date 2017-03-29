484bde0e32947afeb3276ac5bf9452170f0dd529
KateYou <kyoungbe@gmail.com>
2017-03-29 19:44:15 -0400

    loanDetails change date to datetime in database design

--- SingularityFAAST/SingularityDB/dbo/LoanDetails.sql
+++ SingularityFAAST/SingularityDB/dbo/LoanDetails.sql
@@ -3,7 +3,7 @@
 	[LoanDetailId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [LoanMasterId] INT NOT NULL,
 
-	[LoanDate] DATE NOT NULL, 
+	[LoanDate] DATETIME NOT NULL, 
     [LoanDuration] INT NOT NULL DEFAULT 28, 
     [InventoryItemId] INT NOT NULL,
 	[Purpose] VARCHAR(80) NOT NULL,
