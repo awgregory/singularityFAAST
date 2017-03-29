1bdd24345b1808a20c4d5fb0e4f2b20cbadc8eae
Nick <necampanini@gmail.com>
2017-03-29 16:50:08 -0400

    introduce jquery and sample error handling to Report index

--- SingularityFAAST/SingularityFAAST.WebUI/Scripts/Reports/ReportRange.js
+++ SingularityFAAST/SingularityFAAST.WebUI/Scripts/Reports/ReportRange.js
@@ -1,27 +1,26 @@
-﻿//(function ($) {
-//    //on document loaded
-//    $().ready(function () {
-//        //on button click, do the function defined here
-//        $('#submitLoanReport').on('click', function () {
-//            //grab date values
-//            var startDate = $('#startDate').val();
-//            var endDate = $('#endDate').val();
+﻿(function ($) {
+    //on document loaded
+    $().ready(function () {
+        var errorText = $('#error-text');
 
-//            //date objects to compare - created from above values
-//            var start = new Date(startDate).getTime();
-//            var end = new Date(endDate).getTime();
+        errorText.hide();
 
-//            if (start > end) { //error
-//                alert('ERROR')
-//                console.log('error error')
-//            }
-//        })
-//    });
-//})(window.jQuery);
+        //on button click, do the function defined here
+        $('#submitLoanReport').on('click', function (e) {
+            //grab date values
+            var startDate = $('#startDate').val();
+            var endDate = $('#endDate').val();
 
-(function () {
-    window.jQuery(document).ready(function ($) {
-        console.log($)
+            //date objects to compare - created from above values
+            var start = new Date(startDate).getTime();
+            var end = new Date(endDate).getTime();
+
+            if (true) { //replace this with the proper logic
+                console.log('condition hit')
+                errorText.show();
+                e.preventDefault(); //e is the event originating from submit button click
+                //if we hit the error, prevent submission
+            }
+        })
     });
-
 })(window.jQuery);
