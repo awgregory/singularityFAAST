417f46f64ed37e8b8752235f01451aeef94b13cf
Nick <necampanini@gmail.com>
2017-03-29 16:38:22 -0400

    wip

---
+++ SingularityFAAST/SingularityFAAST.WebUI/Scripts/Reports/ReportRange.js
@@ -0,0 +1,27 @@
+﻿//(function ($) {
+//    //on document loaded
+//    $().ready(function () {
+//        //on button click, do the function defined here
+//        $('#submitLoanReport').on('click', function () {
+//            //grab date values
+//            var startDate = $('#startDate').val();
+//            var endDate = $('#endDate').val();
+
+//            //date objects to compare - created from above values
+//            var start = new Date(startDate).getTime();
+//            var end = new Date(endDate).getTime();
+
+//            if (start > end) { //error
+//                alert('ERROR')
+//                console.log('error error')
+//            }
+//        })
+//    });
+//})(window.jQuery);
+
+(function () {
+    window.jQuery(document).ready(function ($) {
+        console.log($)
+    });
+
+})(window.jQuery);
