6555fc52d93adc0e443893a5d89ae2c5e0fb6800
Adrian Gregory <adrianwgregory2@gmail.com>
2017-04-11 22:25:56 -0400

    Getting basic pagination on index page

---
+++ SingularityFAAST/SingularityFAAST.Core/ViewModels/ClientIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SingularityFAAST.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingularityFAAST.Core.ViewModels
+{
+    public class ClientIndexViewModel
+    {
+        public IEnumerable<Client> Clients { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
