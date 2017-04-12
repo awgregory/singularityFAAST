b75e06da7cac6a32ddb13bbea7cd4c6d36691455
Adrian Gregory <adrianwgregory2@gmail.com>
2017-04-12 19:32:59 -0400

    Clean up code in Controller

--- SingularityFAAST/SingularityFAAST.WebUI/Controllers/ClientController.cs
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/ClientController.cs
@@ -16,7 +16,8 @@ namespace SingularityFAAST.WebUI.Controllers
         private readonly int _pageSize = 5;
 
 
-        //  Returns Index View
+        #region Index Get Method - With Paging Logic
+        
         [HttpGet]
         public ActionResult Index(int page = 1)
         {
@@ -40,9 +41,11 @@ namespace SingularityFAAST.WebUI.Controllers
 
             return View(viewModel);
         }
+        #endregion
 
 
-        //  Search Function
+        #region Index Post Method - For Search Function
+
         [HttpPost]
         public ActionResult Index(SearchRequest searchRequest) // Create client 'No Match'
         {
@@ -51,20 +54,22 @@ namespace SingularityFAAST.WebUI.Controllers
             return View(model);
         }
 
+        #endregion
 
 
-
+        #region AddClient Get Method
         //  Returns Add New Client Page
         public ViewResult AddClient()
         {
             return View();
         }
+        #endregion
 
 
-
+        #region AddClient Post Method
         //  Collects form data from AddClient page and saves it
         [HttpPost]
-        public RedirectToRouteResult AddClient(Client client)  
+        public RedirectToRouteResult AddClient(Client client)
         {
             _clientServices.SaveClient(client);
 
@@ -89,42 +94,53 @@ namespace SingularityFAAST.WebUI.Controllers
         //}
         #endregion
 
+        #endregion
 
 
+        
 
-        //  Returns Edit Client Page
+
+        #region EditClient Get Method
+        //  Returns the populated EditClient View,  F12 into Services for details
         [HttpGet]
         public ActionResult EditClient(int id)
         {
-            var client = _clientServices.GetClientDetails(id); 
+            var client = _clientServices.GetClientDetails(id);
 
-            var disabilityList = _clientServices.GetAllDisabilities(); 
+            var disabilityList = _clientServices.GetAllDisabilities();
 
-            IEnumerable<LoanMaster> associatedLoans = _clientServices.GetLoansByClientId(id); 
+            IEnumerable<LoanMaster> associatedLoans = _clientServices.GetLoansByClientId(id);
 
             var viewModel = new EditClientViewModel(client, disabilityList, associatedLoans);
 
-            return View(viewModel); 
+            return View(viewModel);
         }
+        #endregion
 
 
-        //  Collects form data from Edit Client page and saves it
+        #region EditClient Post Method
+        // Model Binder didn't know what to do with the DisabilityIds values until we gave it a parameter that matched the name attribute
         [HttpPost]
-        public ActionResult EditClient(Client client, IEnumerable<int> DisabilityIds)   // Model Binder didn't know what to do with the DisabilityIds values until we gave it a parameter that matched the name attribute
+        public ActionResult EditClient(Client client, IEnumerable<int> DisabilityIds)   
         {
-            _clientServices.EditClientDetails(client, DisabilityIds);  
+            _clientServices.EditClientDetails(client, DisabilityIds);
 
             var clientId = client.ClientID;
 
             return RedirectToAction("EditClient", new { clientId });
 
-            
+
             //sidenote: was working with default return View() before, MVC Convention that it goes back to the original HttpGet which already had the id provided  
         }
 
+        #endregion
 
 
 
+
+
+        #region Delete Get Method
+        //Sets IsDeleted Column Value to true
         [HttpGet]
         public ActionResult Delete(int id)
         {
@@ -132,6 +148,11 @@ namespace SingularityFAAST.WebUI.Controllers
 
             return RedirectToAction("Index", "Client");
         }
+
+        #endregion
+
+
+
 
 
     }
