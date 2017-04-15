ffc33a6e4994827e6b47c0ff2d3015414c6be325
Nick <necampanini@gmail.com>
2017-04-15 15:20:40 -0400

    pair with grayson for excel export prototype

---
+++ SingularityFAAST/SingularityFAAST.Services/Services/ExportServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SingularityFAAST.Core.Entities;
+
+namespace SingularityFAAST.Services.Services
+{
+    public class ExportServices
+    {
+        public string CreateClientListCSV(IList<Client> clients)
+        {
+            var htmlContent = new StringBuilder();
+
+            htmlContent.AppendFormat(
+                "{0}, {1}, {2}",
+                "First Namae",
+                "Last Name",
+                "Email");
+
+            htmlContent.AppendLine();
+
+            foreach (var client in clients)
+            {
+                htmlContent.AppendLine(
+                    client.FirstName + ", " +
+                    client.LastName + ", " +
+                    client.Email
+                );
+            }
+
+            return htmlContent.ToString();
+        }
+    }
+}
---
+++ SingularityFAAST/SingularityFAAST.WebUI/Controllers/ExportController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using SingularityFAAST.Services.Services;
+
+namespace SingularityFAAST.WebUI.Controllers
+{
+    public class ExportController : Controller
+    {
+        private readonly ClientServices clientServices;
+        private readonly ExportServices exportServices;
+
+        public ExportController()
+        {
+            clientServices = new ClientServices();
+            exportServices = new ExportServices();
+        }
+
+        public void ClientReport()
+        {
+            var clients = clientServices.GetAllClients();
+
+            var excelContent = exportServices.CreateClientListCSV(clients);
+
+            //download
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=ClientListExport.csv");
+            Response.Charset = "";
+            Response.ContentType = "application/text";
+            Response.Output.Write(excelContent);
+
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
