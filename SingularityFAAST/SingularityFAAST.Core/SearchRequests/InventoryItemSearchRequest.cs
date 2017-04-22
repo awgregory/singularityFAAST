2cce0c4c5684c24acc8d5b3e4590e1de2cbac08c
Nick <necampanini@gmail.com>
2017-04-22 16:46:30 -0400

    pair with John on search request on inventory item

---
+++ SingularityFAAST/SingularityFAAST.Core/SearchRequests/InventoryItemSearchRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SingularityFAAST.Core.SearchRequests
+{
+    public class InventoryItemSearchRequest
+    {
+        public string FilterByItemName { get; set; }
+
+        public string FilterByItemNumber { get; set; }
+    }
+}
