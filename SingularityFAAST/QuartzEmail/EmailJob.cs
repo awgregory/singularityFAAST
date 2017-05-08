c5da848f832e71c66cd10664eefeef6945a9e722
KateYou <kyoungbe@gmail.com>
2017-05-08 16:46:53 -0400

    scheduler setup - 2nd go

---
+++ SingularityFAAST/QuartzEmail/EmailJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using SingularityFAAST.Services.Services;
+
+
+namespace QuartzEmail
+{
+    //class EmailJob
+    //{
+    //    {
+    //The Job listener
+    public class EmailJob : IJob
+        {
+            public void Execute(IJobExecutionContext context)
+            {
+                LoanMasterServices lm_services = new LoanMasterServices();
+                lm_services.NotifyEmail();
+                Console.WriteLine("Call Email");
+                Debug.WriteLine("Call Email");
+
+            }
+        }
+    }
+
---
+++ SingularityFAAST/QuartzEmail/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Quartz;
+using Quartz.Impl;
+using Quartz.Job;
+using System.Diagnostics;
+
+namespace QuartzEmail
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+
+        {
+            try
+            {
+                Common.Logging.LogManager.Adapter = new Common.Logging.Simple.ConsoleOutLoggerFactoryAdapter
+                {
+                    Level = Common.Logging.LogLevel.Info
+                };
+
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+                Debug.WriteLine("step 1");
+                // and start it off
+                scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<EmailJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(120)
+                        .RepeatForever())
+                    .Build();
+
+                // Trigger the job to run at 9 am every day
+                //ITrigger trigger = TriggerBuilder.Create()
+                //    .WithIdentity("trigger1", "group1")
+                //    .ForJob("job1")
+                //    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(9, 0))
+                //    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                scheduler.ScheduleJob(job, trigger);
+                Console.WriteLine("Job scheduled");
+                Debug.WriteLine("Job scheduled");
+                // some sleep to show what's happening
+                //Thread.Sleep(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+
+        }
+
+    }
+}
